cb68fd7d4c6879be6e15a5e1ce77efab7971ae7c
priyyadav <yadavpriyanka9628@gmail.com>
2024-03-06 21:44:43 +0530

    create user store

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './Login'
 import { Browse } from './Browse'
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '../utils/firebase';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
 export const Body = () => {
+    const dispatch=useDispatch()
+
     const appRouter=createBrowserRouter([
         {
             path:"/",
@@ -13,6 +19,9 @@ export const Body = () => {
             element:<Browse></Browse>
         }
     ])
+
+  
+   
   return (
     <div>
         <RouterProvider router={appRouter}></RouterProvider>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,44 @@
-import React from 'react'
-import { LOGO } from '../utils/constant'
-export const Header = () => {
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { LOGO } from "../utils/constant";
+import { auth } from "../utils/firebase";
+import { addUser, removeUser } from "../utils/userSlice";
+
+
+const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  // const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate('/')
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
+
   return (
-    <div className='absolute px-2 py-2 bg-gradient-to-b from-black z-10'>
-        <img  className="w-44"
-        src={LOGO} alt='logo'/>
-    </div>
-  )
-}
+    <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
+      <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
+
+     
+          <img
+            className="hidden md:block w-12 h-12"
+            alt="usericon"
+               src={user?.photoURL}
+          />
+          <button onClick={handleSignOut} className="font-bold text-white ">
+            (Sign Out)
+          </button>
+        </div>
+
+  );
+};
+export default Header;
