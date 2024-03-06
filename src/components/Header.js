c52a682d8fbb0ff252a8382f08d9d6555a9f002f
priyyadav <yadavpriyanka9628@gmail.com>
2024-03-06 23:03:15 +0530

    build issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,29 +16,79 @@ const Header = () => {
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
-        navigate('/')
+        
       })
       .catch((error) => {
         navigate("/error");
       });
   };
 
+  useEffect(() => {
+   onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+
+    // Unsiubscribe when component unmounts
+    
+  }, []);
+
+  // const handleGptSearchClick = () => {
+  //   // Toggle GPT Search
+  //   dispatch(toggleGptSearchView());
+  // };
+
+  // const handleLanguageChange = (e) => {
+  //   dispatch(changeLanguage(e.target.value));
+  // };
 
   return (
-    <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
       <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
-
-     
+      {user && (
+        <div className="flex p-2 justify-between">
+          {/* {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )} */}
+          {/* <button
+            className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
+            onClick={handleGptSearchClick}
+          >
+            {showGptSearch ? "Homepage" : "GPT Search"}
+          </button> */}
           <img
             className="hidden md:block w-12 h-12"
             alt="usericon"
-               src={user?.photoURL}
+            src={user?.photoURL}
           />
           <button onClick={handleSignOut} className="font-bold text-white ">
             (Sign Out)
           </button>
         </div>
-
+      )}
+    </div>
   );
 };
 export default Header;
